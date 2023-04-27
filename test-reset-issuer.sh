9f7139c030e05e77ee5cf09ae50fce9ccd33b017
Alex Walker <alex.walker@indicio.tech>
2023-04-27 09:10:29 -0700

    Update test-reset-issuer.sh

--- test-reset-issuer.sh
+++ test-reset-issuer.sh
@@ -5,20 +5,21 @@ workers35=(3 5)
 workers1=(1)
 
 function reset_issuer () {
-  # Takes in duration (1), workers (3+)
-  duration=$1 && shift
-  workers=("$@")
+	# Takes in duration (1), workers (3+)
+	duration=$1 && shift
+	workers=("$@")
 	for item in "${workers[@]}" 
 	do
-    sleep ${duration}
-    sleep 450
-    sudo docker compose -f issuer-docker-compose.yml down -v
-    sudo docker compose -f issuer-docker-compose.yml up -d
-    sleep 450
-    sleep 900 
+		sleep ${duration}
+		sleep 450
+		sudo docker compose -f issuer-docker-compose.yml down -v
+		sudo docker compose -f issuer-docker-compose.yml up -d
+		sleep 450
+		sleep 900 
 	done
 }
 
+reset_issuer 28800 "${workers1[@]}"
 reset_issuer 28800 "${workers1[@]}"
 reset_issuer 28800 "${workers1[@]}"
 reset_issuer 28800 "${workers1[@]}"
